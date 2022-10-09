d255803402e80273cfe87bca01b1311f4061158d
Rodolfo Martínez Anaya <rorromart@gmail.com>
2022-10-09 12:19:57 -0500

    Jalando en windows cmd y powershell

---
+++ imp.py
@@ -0,0 +1,36 @@
+from this import d
+from tkinter import image_names
+import PIL
+from PIL import Image
+from tkinter.filedialog import *
+import os
+
+# Getting a touple of file images
+files = askopenfilenames()
+
+try:
+  # Getting the path of working directory
+  path = os.getcwd()
+
+  # Create a directory for compressed images
+  img_compress_directory = path+"\img-compressed"
+  if not os.path.exists(img_compress_directory):
+    os.makedirs(img_compress_directory)
+
+  # Moving tho directory for compressed images
+  os.chdir(img_compress_directory)
+
+  # Iterating the files
+  for i in range(len(files)):
+    image = Image.open(files[i])
+
+    # Show the filename of a image in touple
+    print("Compressing image:", image.filename[-9:])
+
+    # Compressing every image in the touple
+    image.save(f"comprimida{i}.jpg", "JPEG", optimize=True, quality=10)
+
+  print("All images are compressed rigth")
+  os.system("start .")
+except:
+  print("Some part in the compression procces brack")
